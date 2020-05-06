ea5244adc35a061cbc06d48c351f398bdc3d5fc3
Dhruva Sagar <dhruva.sagar@gmail.com>
2020-05-06 19:33:48 +0530

    HackerRank : Migratory Birds

--- hackerrank/practice/migratory_birds/migratory_birds.hs
+++ hackerrank/practice/migratory_birds/migratory_birds.hs
@@ -1,10 +1,13 @@
 import Data.Ord
 import Data.List
+import Data.Function
 
 mostCommon :: Ord a => [a] -> a
-mostCommon = snd . head . maximum . groupBy (\a b -> fst a == fst b) . map (\xs -> (length xs, head xs)) . group . sort
+-- mostCommon = snd . head . maximum . groupBy (\a b -> fst a == fst b) . map (\xs -> (length xs, head xs)) . group . sort
+mostCommon = head . head . sortBy (flip compare `on` length) . group . sort
 
 solve :: [Int] -> String
 solve = show . mostCommon
 
+main :: IO ()
 main = interact $ solve . map read . tail . words
