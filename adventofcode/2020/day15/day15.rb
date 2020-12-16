e2b41cb2de2d75522bae6d996e4e14cd2e3f7b19
Dhruva Sagar <dhruva.sagar@gmail.com>
2020-12-16 10:12:52 +0530

    AOC 2020 : Day15 faster ruby solution

--- adventofcode/2020/day15/day15.rb
+++ adventofcode/2020/day15/day15.rb
@@ -41,6 +41,37 @@ def part(numbers, limit)
   end
 end
 
+def fast_part(numbers, limit)
+  lomem_size = 10_000_000
+  mem = {}
+  lomem = Array.new(lomem_size) { 0 }
+
+  numbers.each.with_index { |n, i| lomem[n] = i + 1 }
+  num = numbers[-1]
+  turn = numbers.size
+
+  while turn != limit
+    m = -1
+    ok = false
+    if num < lomem_size
+      m = lomem[num]
+      ok = m != 0
+      lomem[num] = turn
+    else
+      m = mem[num]
+      ok = mem.key?(num)
+      mem[num] = turn
+    end
+    num = if ok
+            turn - m
+          else
+            0
+          end
+    turn += 1
+  end
+  num
+end
+
 start_numbers = read_input[0].split(',').map(&:to_i)
-p part(start_numbers, PART1_LIMIT)
-p part(start_numbers, PART2_LIMIT)
+p fast_part(start_numbers, PART1_LIMIT)
+p fast_part(start_numbers, PART2_LIMIT)
