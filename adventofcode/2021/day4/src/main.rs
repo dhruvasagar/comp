2c200506e59d169222d7563cce00a385c015d444
Dhruva Sagar <dhruva.sagar@gmail.com>
2021-12-05 02:29:40 +0530

    AOC 2021 : Day4 : Minor refactor

--- adventofcode/2021/day4/src/main.rs
+++ adventofcode/2021/day4/src/main.rs
@@ -40,7 +40,7 @@ impl Board {
         rowwon || colwon
     }
 
-    fn score(&self) -> i32 {
+    fn score(&self, num: i32) -> i32 {
         let bsize = self.cells.len();
         let mut res: i32 = 0;
         for i in 0..bsize {
@@ -51,28 +51,10 @@ impl Board {
                 }
             }
         }
-        res
+        res * num
     }
 
-    async fn play(&mut self, nums: Vec<i32>, sx: tokio::sync::mpsc::Sender<i32>) {
-        let bsize = self.cells.len();
-        for num in nums {
-            for i in 0..bsize {
-                for j in 0..bsize {
-                    let cell = self.cells.get_mut(i).unwrap().get_mut(j).unwrap();
-                    if cell.num == num {
-                        cell.marked = true;
-                    }
-                    if self.has_won() {
-                        sx.send(num * self.score()).await;
-                        return;
-                    }
-                }
-            }
-        }
-    }
-
-    fn playnum(&mut self, num: i32) {
+    fn play(&mut self, num: i32) {
         let bsize = self.cells.len();
         for i in 0..bsize {
             for j in 0..bsize {
@@ -86,13 +68,12 @@ impl Board {
     }
 }
 
-fn part1(lines: Vec<String>) -> String {
-    let nums: Vec<i32> = lines[0].split(',').map(|x| x.parse().unwrap()).collect();
+fn parse_input(lines: Vec<String>) -> Vec<Board> {
     let mut boards: Vec<Board> = vec![];
-    let mut idx = 2;
+    let mut idx = 0;
     while idx < lines.len() {
         let mut board = Board::default();
-        board.id = (idx as i32 - 2) / 6; // 6 due to the empty line
+        board.id = (idx as i32) / 6; // 6 due to the empty line
         while idx < lines.len() {
             let line = lines[idx].clone();
             if line.is_empty() {
@@ -113,69 +94,48 @@ fn part1(lines: Vec<String>) -> String {
         boards.push(board);
         idx += 1;
     }
+    boards
+}
 
-    // let (sx, mut rx) = tokio::sync::mpsc::channel::<i32>(1);
-    // for mut board in boards {
-    //     let nums = nums.clone();
-    //     let sender = sx.clone();
-    //     tokio::spawn(async move {
-    //         board.play(nums.clone(), sender).await;
-    //     });
-    // }
-    // format!("{}", rx.recv().await.unwrap())
-    for num in nums {
+fn part1(lines: Vec<String>) -> String {
+    let nums: Vec<i32> = lines[0].split(',').map(|x| x.parse().unwrap()).collect();
+    let mut boards = parse_input(lines[2..].to_vec());
+
+    let mut score: i32 = 0;
+    'outer: for num in nums {
         for board in boards.iter_mut() {
-            board.playnum(num);
+            board.play(num);
             if board.has_won() {
-                return format!("{}", num * board.score());
+                score = board.score(num);
+                break 'outer;
             }
         }
     }
-    format!("part1")
+    format!("{}", score)
 }
 
 fn part2(lines: Vec<String>) -> String {
     let nums: Vec<i32> = lines[0].split(',').map(|x| x.parse().unwrap()).collect();
-    let mut boards: Vec<Board> = vec![];
-    let mut idx = 2;
-    while idx < lines.len() {
-        let mut board = Board::default();
-        board.id = (idx as i32 - 2) / 6; // 6 due to the empty line
-        while idx < lines.len() {
-            let line = lines[idx].clone();
-            if line.is_empty() {
-                break;
-            }
-            let row: Vec<i32> = line
-                .split_whitespace()
-                .map(|x| x.parse().unwrap())
-                .collect();
-            let mut cell_row: Vec<Cell> = vec![];
-            for num in row {
-                let cell = Cell { num, marked: false };
-                cell_row.push(cell);
-            }
-            board.cells.push(cell_row);
-            idx += 1;
-        }
-        boards.push(board);
-        idx += 1;
-    }
-    let mut last_winning_num: i32 = 0;
-    let mut last_winning_score: i32 = 0;
+    let mut boards = parse_input(lines[2..].to_vec());
+
+    let mut score: i32 = 0;
     for num in nums {
+        let mut done = true;
         for board in boards.iter_mut() {
             if board.has_won() {
                 continue;
             }
-            board.playnum(num);
+            board.play(num);
             if board.has_won() {
-                last_winning_num = num;
-                last_winning_score = board.score();
+                score = board.score(num);
             }
+            done &= board.has_won();
+        }
+        if done {
+            break;
         }
     }
-    return format!("{}", last_winning_num * last_winning_score)
+    format!("{}", score)
 }
 
 #[tokio::main]
