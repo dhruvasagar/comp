8dfcee7422086096de105e57507be7aa883219f5
Dhruva Sagar <dhruva.sagar@gmail.com>
2021-12-06 12:24:56 +0530

    AOC 2021 : Day6 : Minor refactor

--- adventofcode/2021/day6/src/main.rs
+++ adventofcode/2021/day6/src/main.rs
@@ -14,24 +14,10 @@ fn read_input() -> Vec<i32> {
         .collect()
 }
 
-#[derive(Clone, Debug)]
-struct LanternFish {
-    timer: i32,
-}
-
-fn parse_input(lines: Vec<i32>) -> Vec<LanternFish> {
-    let mut fishes: Vec<LanternFish> = vec![];
-    for timer in lines {
-        let fish = LanternFish { timer };
-        fishes.push(fish);
-    }
-    fishes
-}
-
-fn simulate(fishes: Vec<LanternFish>) -> (i64, i64) {
+fn simulate(fishes: Vec<i32>) -> (i64, i64) {
     let mut timers = vec![0; 9];
     for f in fishes {
-        timers[f.timer as usize] += 1;
+        timers[f as usize] += 1;
     }
     let mut part1 = 0;
     for day in 0..256 {
@@ -42,14 +28,14 @@ fn simulate(fishes: Vec<LanternFish>) -> (i64, i64) {
         for i in 0..8 {
             timers[i] = timers[i + 1];
         }
-        timers[8] = zero;
-        timers[6] += zero; // each fish at 0 timer creates a new fish
+        timers[8] = zero; // each fish at 0 timer creates a new fish
+        timers[6] += zero; // the old fish timer now resets to 6
     }
     (part1, timers.iter().sum())
 }
 
 fn main() {
-    let fishes = parse_input(read_input());
+    let fishes = read_input();
     let (part1, part2) = simulate(fishes);
     println!("{}", part1);
     println!("{}", part2);
