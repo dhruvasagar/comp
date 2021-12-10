e6d903b9c3e6d356b84775ba985b4bd2b5890260
Dhruva Sagar <dhruva.sagar@gmail.com>
2021-12-10 11:08:36 +0530

    AOC 2021 : Day10 : Ruby

---
+++ adventofcode/2021/day10/day10.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+def read_input
+  ARGF.readlines
+end
+
+PAIRS = { '}' => '{', ']' => '[', ')' => '(', '>' => '<' }.freeze
+REV_PAIRS = { '{' => '}', '[' => ']', '(' => ')', '<' => '>' }.freeze
+SYNTAX_ERROR_SCORE = { ')' => 3, ']' => 57, '}' => 1197, '>' => 25_137 }.freeze
+AUTOCOMPLETE_SCORE = { ')' => 1, ']' => 2, '}' => 3, '>' => 4 }.freeze
+
+def corrupted?(line)
+  stack = []
+  line.each do |c|
+    case c
+    when '{', '[', '(', '<'
+      stack.push(c)
+    when '}', ']', ')', '>'
+      return c if stack.last != PAIRS[c]
+
+      stack.pop
+    end
+  end
+  stack
+end
+
+lines = read_input.map { |l| l.chomp.chars }
+lcs = lines.map(&method(:corrupted?))
+
+p lcs.select { |l| l.size == 1 }.map { |l| SYNTAX_ERROR_SCORE[l] }.sum
+
+def p2_score(bcs)
+  bcs.reverse.map { |c| REV_PAIRS[c] }.reduce(0) { |r, c| 5 * r + AUTOCOMPLETE_SCORE[c] }
+end
+
+ss = lcs.reject { |l| l.size == 1 }.map(&method(:p2_score)).sort
+p ss[ss.size / 2]
