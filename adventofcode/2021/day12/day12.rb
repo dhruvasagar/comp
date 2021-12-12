1ca7343d6dd10f831d69048e3dfd8dace1ddfb28
Dhruva Sagar <dhruva.sagar@gmail.com>
2021-12-12 12:32:47 +0530

    AOC 2021 : Day12 : Ruby

---
+++ adventofcode/2021/day12/day12.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+def read_input
+  ARGF.readlines
+end
+
+def small?(char)
+  /[[:lower:]]/.match(char)
+end
+
+def all_paths(graph, start, ends, path, &block)
+  path += [start]
+
+  block.call(path) if start == ends
+
+  graph[start].each do |np|
+    all_paths(graph, np, ends, path, &block) if !small?(np) || !path.include?(np)
+  end
+end
+
+def part1(graph)
+  paths = []
+  all_paths(graph, 'start', 'end', []) do |path|
+    paths.push(path)
+  end
+  paths.size
+end
+
+def valid_path?(path, node)
+  return true unless path.include?(node)
+
+  hc = Hash.new { |h, k| h[k] = 0 }
+  path.select(&method(:small?)).each { |p| hc[p] += 1 }
+
+  return false if hc[node] > 1
+
+  vals = hc.values
+  diff = vals.sum - vals.size
+  diff.zero?
+end
+
+def all_paths2(graph, start, ends, path, &block)
+  path += [start]
+
+  block.call(path) if start == ends
+
+  graph[start].each do |np|
+    all_paths2(graph, np, ends, path, &block) if !small?(np) || (np != 'start' && valid_path?(path, np))
+  end
+end
+
+def part2(graph)
+  paths = []
+  all_paths2(graph, 'start', 'end', []) do |path|
+    paths.push(path)
+  end
+  paths.size
+end
+
+graph = Hash.new { |h, k| h[k] = [] }
+read_input.each do |l|
+  s, e = l.chomp.split('-')
+  graph[s] << e unless e == 'start' || s == 'end'
+  graph[e] << s unless s == 'start' || e == 'end'
+end
+p part1(graph)
+p part2(graph)
