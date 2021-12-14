e2852ae76fb0794daff183290e33a89467a7697b
Dhruva Sagar <dhruva.sagar@gmail.com>
2021-12-15 01:23:06 +0530

    AOC 2021 : Day14 : Minor refactor to ruby code

--- adventofcode/2021/day14/day14.rb
+++ adventofcode/2021/day14/day14.rb
@@ -4,31 +4,12 @@ def read_input
   ARGF.readlines
 end
 
-def expand(poly, rules)
-  cs = poly.chars
-  cs.each.map.with_index do |c, i|
-    next [cs[i]] if i == cs.size - 1
-
-    [c, rules[c + cs[i + 1]]].join
-  end.join
-end
-
-def part1(poly, rules)
-  np = 10.times.to_a.reduce(poly) { |p| expand(p, rules) }
-  hc = Hash.new { |h, k| h[k] = 0 }
-  np.chars.each_with_object(hc) { |c, h| h[c] += 1 }
-  vals = hc.values
-  vals.max - vals.min
-end
-
 lines = read_input
 poly = lines[0].chomp
 rules = lines[2...lines.size].each_with_object({}) do |l, m|
   pair, ins = l.chomp.split(' -> ')
   m[pair] = ins
 end
-
-# p part1(poly, rules)
 
 def pair_freq(hcs, rules, count)
   count.times do
@@ -42,19 +23,16 @@ def pair_freq(hcs, rules, count)
   hcs
 end
 
-def letter_freq(hcs, last)
+def letter_freq(hcs)
   lc = Hash.new { |h, k| h[k] = 0 }
-  hcs.each_with_object(lc) do |(p, c), h|
-    h[p[0]] += c #hcs[p]
-  end
-  lc[last] += 1
-  lc
+  hcs.each_with_object(lc) { |(p, c), h| h[p[0]] += c }
 end
 
 def part2(poly, rules, count)
   hc = Hash.new { |h, k| h[k] = 0 }
   (0...(poly.size - 1)).each_with_object(hc) { |i, h| h[poly[i..(i + 1)]] += 1 }
-  lc = letter_freq(pair_freq(hc, rules, count), poly[-1])
+  lc = letter_freq(pair_freq(hc, rules, count))
+  lc[poly[-1]] += 1
   vals = lc.values
   vals.max - vals.min
 end
