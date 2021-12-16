c57650c5009b007ca18c6b8b1da241677b257ff4
Dhruva Sagar <dhruva.sagar@gmail.com>
2021-12-16 15:25:53 +0530

    AOC 2021 : Day16 : Rust

---
+++ adventofcode/2021/day16/src/main.rs
@@ -0,0 +1,303 @@
+use std::io;
+use std::str::FromStr;
+use std::time::Instant;
+
+fn read_input() -> String {
+    use std::io::prelude::*;
+    let stdin = io::stdin();
+    stdin
+        .lock()
+        .lines()
+        .map(|x| x.unwrap())
+        .collect::<Vec<String>>()[0]
+        .chars()
+        .map(|l| format!("{:04b}", l.to_digit(16).unwrap()))
+        .collect::<Vec<String>>()
+        .join("")
+}
+
+#[allow(dead_code)]
+#[derive(Clone, Debug)]
+enum Operator {
+    FifteenBitTotalLength { length: i64 },
+    ElevenBitPacketCount { count: i64 },
+}
+
+#[allow(dead_code)]
+#[derive(Clone, Debug)]
+enum PacketType {
+    LiteralValue {
+        value: i64,
+        version: i64,
+        last_index: usize,
+    },
+    Operator {
+        nums: Vec<i64>,
+        type_id: i64,
+        version: i64,
+        versions: Vec<i64>,
+        operator: Operator,
+        last_index: usize,
+        packets: Vec<PacketType>,
+    },
+}
+
+impl PacketType {
+    fn version_total(&self) -> i64 {
+        match self {
+            PacketType::LiteralValue {
+                version,
+                value: _,
+                last_index: _,
+            } => *version,
+            PacketType::Operator {
+                versions,
+                nums: _,
+                type_id: _,
+                version: _,
+                operator: _,
+                last_index: _,
+                packets: _,
+            } => versions.iter().sum(),
+        }
+    }
+    fn value(&self) -> i64 {
+        match self {
+            PacketType::LiteralValue {
+                value,
+                version: _,
+                last_index: _,
+            } => *value,
+            PacketType::Operator {
+                type_id,
+                packets,
+                nums: _,
+                version: _,
+                versions: _,
+                operator: _,
+                last_index: _,
+            } => {
+                match type_id {
+                    0 => {
+                        // sum
+                        packets.iter().map(|p| p.value()).sum()
+                    }
+                    1 => {
+                        // product
+                        packets.iter().map(|p| p.value()).product()
+                    }
+                    2 => {
+                        // minimum
+                        packets.iter().map(|p| p.value()).min().unwrap()
+                    }
+                    3 => {
+                        // maximum
+                        packets.iter().map(|p| p.value()).max().unwrap()
+                    }
+                    5 => {
+                        // greater than
+                        let p1 = packets[0].clone();
+                        let p2 = packets[1].clone();
+                        if p1.value() > p2.value() {
+                            1
+                        } else {
+                            0
+                        }
+                    }
+                    6 => {
+                        // less than
+                        let p1 = packets[0].clone();
+                        let p2 = packets[1].clone();
+                        if p1.value() < p2.value() {
+                            1
+                        } else {
+                            0
+                        }
+                    }
+                    7 => {
+                        // equal to
+                        let p1 = packets[0].clone();
+                        let p2 = packets[1].clone();
+                        if p1.value() == p2.value() {
+                            1
+                        } else {
+                            0
+                        }
+                    }
+                    _ => 0,
+                }
+            }
+        }
+    }
+}
+
+impl FromStr for PacketType {
+    type Err = ();
+    fn from_str(s: &str) -> Result<PacketType, ()> {
+        let version = i64::from_str_radix(&s[0..=2], 2).unwrap();
+        let type_id = i64::from_str_radix(&s[3..=5], 2).unwrap();
+        // println!("s: {}, version: {}, type_id: {}", s, version, type_id);
+        match type_id {
+            4 => {
+                let size = 5;
+                let mut start = 6;
+                let mut bits: String = String::new();
+                while start < s.len() {
+                    let end = start + size;
+                    if end > s.len() {
+                        break;
+                    }
+
+                    let sbit = s.chars().nth(start).unwrap();
+                    bits += &s[(start + 1)..end].to_string();
+                    start += size;
+                    if sbit == '0' {
+                        break;
+                    }
+                }
+                let value = i64::from_str_radix(bits.as_str(), 2).unwrap();
+                Ok(PacketType::LiteralValue {
+                    value,
+                    version,
+                    last_index: start,
+                })
+            }
+            _ => {
+                // Operator
+                match &s[6..7] {
+                    "0" => {
+                        // parse 15 bits as total length of bits in subpackets
+                        let length = i64::from_str_radix(&s[7..22], 2).unwrap();
+                        let end = 22 + length as usize;
+                        let mut index = 22;
+                        let mut nums: Vec<i64> = vec![];
+                        let mut vers: Vec<i64> = vec![version];
+                        let mut packets: Vec<PacketType> = vec![];
+                        while index < end {
+                            let p = PacketType::from_str(&s[index..])?;
+                            packets.push(p.clone());
+                            match p {
+                                PacketType::LiteralValue {
+                                    value,
+                                    version,
+                                    last_index,
+                                } => {
+                                    nums.push(value);
+                                    vers.push(version);
+                                    index += last_index;
+                                }
+                                PacketType::Operator {
+                                    last_index,
+                                    versions: vs,
+                                    nums: ns,
+                                    type_id: _,
+                                    version: _,
+                                    operator: _,
+                                    packets: _,
+                                } => {
+                                    for n in ns {
+                                        nums.push(n);
+                                    }
+                                    for v in vs {
+                                        vers.push(v);
+                                    }
+                                    index += last_index;
+                                }
+                            };
+                        }
+
+                        Ok(PacketType::Operator {
+                            nums,
+                            type_id,
+                            version,
+                            packets,
+                            versions: vers,
+                            last_index: end,
+                            operator: Operator::FifteenBitTotalLength { length },
+                        })
+                    }
+                    "1" => {
+                        // parse 11 bits as number of packets
+                        let count = i64::from_str_radix(&s[7..18], 2).unwrap();
+                        let mut index = 18;
+                        let mut nums: Vec<i64> = vec![];
+                        let mut vers: Vec<i64> = vec![version];
+                        let mut packets: Vec<PacketType> = vec![];
+                        let mut pcount = 0;
+                        while pcount < count as usize {
+                            let p = PacketType::from_str(&s[index..])?;
+                            packets.push(p.clone());
+                            match p {
+                                PacketType::LiteralValue {
+                                    value,
+                                    version,
+                                    last_index,
+                                } => {
+                                    nums.push(value);
+                                    vers.push(version);
+                                    index += last_index;
+                                }
+                                PacketType::Operator {
+                                    last_index,
+                                    versions: vs,
+                                    nums: ns,
+                                    version: _,
+                                    operator: _,
+                                    packets: _,
+                                    type_id: _,
+                                } => {
+                                    for n in ns {
+                                        nums.push(n);
+                                    }
+                                    for v in vs {
+                                        vers.push(v);
+                                    }
+                                    index += last_index;
+                                }
+                            }
+                            pcount += 1;
+                        }
+
+                        Ok(PacketType::Operator {
+                            nums,
+                            version,
+                            packets,
+                            type_id,
+                            versions: vers,
+                            last_index: index,
+                            operator: Operator::ElevenBitPacketCount { count },
+                        })
+                    }
+                    _ => Err(()),
+                }
+            }
+        }
+    }
+}
+
+fn part1(packet: PacketType) -> String {
+    format!("{}", packet.version_total())
+}
+
+fn part2(packet: PacketType) -> String {
+    format!("{}", packet.value())
+}
+
+fn main() -> Result<(), ()> {
+    let s = Instant::now();
+    let bits = read_input();
+    let packet = PacketType::from_str(bits.as_str())?;
+
+    let s1 = Instant::now();
+    println!("{}", part1(packet.clone()));
+    let e1 = s1.elapsed();
+    let s2 = Instant::now();
+    println!("{}", part2(packet.clone()));
+    let e2 = s2.elapsed();
+    let e = s.elapsed();
+    println!(
+        "Time for part1: {:?}, part2: {:?}, total with IO: {:?}",
+        e1, e2, e
+    );
+    Ok(())
+}
