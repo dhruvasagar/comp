55c8dd8dc13666cefac40a5cb25cbae4a462dbc1
Dhruva Sagar <dhruva.sagar@gmail.com>
2021-12-20 12:32:29 +0530

    AOC 2021 : Day20 : Ruby (horrible)

---
+++ adventofcode/2021/day20/day20.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+def read_input
+  ARGF.readlines
+end
+
+def pix2bin(pix)
+  pix == '.' ? '0' : '1'
+end
+
+def bin2dec(arr)
+  arr.map(&method(:pix2bin)).join.to_i(2)
+end
+
+def neighs(point)
+  x, y = point
+  [
+    [x - 1, y -1],
+    [x, y - 1],
+    [x + 1, y - 1],
+    [x - 1, y],
+    [x, y],
+    [x + 1, y],
+    [x - 1, y + 1],
+    [x, y + 1],
+    [x + 1, y + 1],
+  ]
+end
+
+def get(image, point, default)
+  x, y = point
+  return default if x < 0 || y < 0
+
+  image[y][x] || default
+rescue
+  default
+end
+
+def enhance(image, algo, count)
+  count.times do |t|
+    range = (-1..image.size)
+    nimage = range.map do |y|
+      range.map do |x|
+        d = t % 2 == 1 ? algo[0] : '.'
+        sq = neighs([x, y]).map do |gx, gy|
+          get(image, [gx, gy], d)
+        end
+        index = bin2dec(sq)
+        algo[index]
+      end
+    end
+    # puts "count: #{t + 1}:"
+    # puts nimage.map(&:join)
+    # puts
+    image = nimage
+  end
+  image
+end
+
+lines = read_input
+algo = lines[0].chomp.chars
+image = lines[2..].map { |l| l.chomp.chars }
+
+# Part 1
+output = enhance(image, algo, 2)
+puts output.map { |r| r.select { |p| p == '#' }.size }.sum
+
+# Part 2
+output = enhance(image, algo, 50)
+puts output.map { |r| r.select { |p| p == '#' }.size }.sum
