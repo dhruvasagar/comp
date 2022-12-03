2282e8293f75d874165368476107f617f76b8b82
Dhruva Sagar <dhruva.sagar@gmail.com>
2022-12-03 11:38:55 +0530

    AOC 2022 : Day 03 : Refactored

--- adventofcode/2022/day03/src/main.rs
+++ adventofcode/2022/day03/src/main.rs
@@ -7,7 +7,7 @@ fn read_input() -> Vec<String> {
     stdin.lock().lines().map(|x| x.unwrap()).collect()
 }
 
-fn find_common_item(items: String) -> String {
+fn find_common_item(items: &String) -> String {
     let mut cs = String::new();
     let mid = items.len() / 2;
     let (left, right) = items.split_at(mid);
@@ -23,42 +23,33 @@ fn priority(c: char) -> u32 {
     if c.is_lowercase() {
         return (c as u32 - 'a' as u32) + 1;
     }
-
-    return (c as u32 - 'A' as u32) + 27;
+    (c as u32 - 'A' as u32) + 27
 }
 
 fn part1(lines: Vec<String>) -> u32 {
-    let mut tot: u32 = 0;
-    for line in lines {
-        let cs = find_common_item(line);
-        let ct = cs.chars().map(|c| priority(c)).sum::<u32>();
-        tot = tot + ct;
-    }
-    tot
+    lines
+        .iter()
+        .map(find_common_item)
+        .fold(0, |tot, cs| tot + cs.chars().map(priority).sum::<u32>())
 }
 
-fn find_common_item_batch(batch: Vec<String>) -> String {
-    let mut cs = String::new();
+fn find_common_item_batch(batch: &[String]) -> String {
     let first = &batch[0];
     let second = &batch[1];
     let third = &batch[2];
-    for c in first.chars() {
-        if second.contains(c) && third.contains(c) && !cs.contains(c) {
-            cs.push(c);
-        }
-    }
-    cs
+    first
+        .chars()
+        .find(|&c| second.contains(c) && third.contains(c))
+        .unwrap()
+        .to_string()
 }
 
 fn part2(lines: Vec<String>) -> u32 {
     const BATCH: usize = 3;
-    let mut tot: u32 = 0;
-    for batch in lines.chunks(BATCH) {
-        let cs = find_common_item_batch(batch.to_vec());
-        let ct = cs.chars().map(|c| priority(c)).sum::<u32>();
-        tot = tot + ct;
-    }
-    tot
+    lines
+        .chunks(BATCH)
+        .map(find_common_item_batch)
+        .fold(0, |tot, cs| tot + cs.chars().map(priority).sum::<u32>())
 }
 
 fn main() {
@@ -68,7 +59,7 @@ fn main() {
     println!("{}", part1(lines.clone()));
     let e1 = s1.elapsed();
     let s2 = Instant::now();
-    println!("{}", part2(lines.clone()));
+    println!("{}", part2(lines));
     let e2 = s2.elapsed();
     let e = s.elapsed();
     println!(
