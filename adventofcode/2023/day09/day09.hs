ede5d82729ccc646013f8c58c0c2919d08fb9558
Dhruva Sagar <dhruva.sagar@gmail.com>
2023-12-09 16:21:12 +0530

    AOC 2023 : Day 09 : Haskell refactor
    So freaking smart!
    Source : https://www.reddit.com/r/haskell/comments/18e64uv/comment/kclkkrs/?utm_source=share&utm_medium=web2x&context=3

--- adventofcode/2023/day09/day09.hs
+++ adventofcode/2023/day09/day09.hs
@@ -1,26 +1,14 @@
 diffs :: [Int] -> [Int]
-diffs [x, y] = [y - x]
-diffs (x : y : xs) = (y - x) : diffs (y : xs)
+diffs xs = zipWith subtract xs (tail xs)
 
-predictNext :: [Int] -> Int
-predictNext ns
-    | all (== 0) ds = head ns
-    | otherwise = (last ns) + predictNext ds
-  where
-    ds = diffs ns
-
-predictPrev :: [Int] -> Int
-predictPrev ns
-    | all (== 0) ds = head ns
-    | otherwise = (head ns) - predictPrev ds
-  where
-    ds = diffs ns
+next :: [Int] -> Int
+next = sum . map last . takeWhile (any (/= 0)) . iterate diffs
 
 part1 :: [[Int]] -> Int
-part1 = sum . map predictNext
+part1 = sum . map next
 
 part2 :: [[Int]] -> Int
-part2 = sum . map predictPrev
+part2 = sum . map (next . reverse)
 
 main :: IO ()
 main = interact $ unlines . map show . sequence [part1, part2] . map (map read . words) . lines
