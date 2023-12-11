dc9390ecd0cdd9a26d5c53ab8c7006755bf3d17a
Dhruva Sagar <dhruva.sagar@gmail.com>
2023-12-11 13:08:57 +0530

    AOC 2023 : Day 11 : Ruby refactor

--- adventofcode/2023/day11/day11.rb
+++ adventofcode/2023/day11/day11.rb
@@ -4,6 +4,7 @@ def read_input
   ARGF.readlines
 end
 
+def tfmt(d) = "#{d/1000000}ms"
 def is_space?(c) = c == '.'
 def is_galaxy?(c) = c == '#'
 def is_empty_row?(row) = row.all?(&method(:is_space?))
@@ -14,7 +15,7 @@ def print(galaxy)
   puts galaxy.map {|row| row.join}.join("\n")
 end
 
-def mdist(galaxy, p1, p2)
+def mdist(p1, p2)
   x1, y1 = p1
   x2, y2 = p2
   (x1 - x2).abs + (y1 - y2).abs
@@ -23,17 +24,24 @@ end
 def dist(galaxy, p1, p2, ris, cis, multiplier)
   (x1, x2), (y1, y2) = [p1[0], p2[0]].minmax, [p1[1], p2[1]].minmax
   ex, ey = ((x1..x2).to_a & cis).size, ((y1..y2).to_a & ris).size
-  mdist(galaxy, p1, p2) + (ex + ey) * (multiplier - 1)
+  mdist(p1, p2) + (ex + ey) * (multiplier - 1)
 end
 
 def sum_dists(galaxy, gps, ris, cis, multiplier)
   gps.combination(2).reduce(0) {|s, (p1, p2)| s + dist(galaxy, p1, p2, ris, cis, multiplier)}
 end
 
+s = Process.clock_gettime(Process::CLOCK_MONOTONIC, :nanosecond)
 galaxy = read_input.map(&:chomp).map(&:chars)
 gps = []
 ris = empty_row_indexes(galaxy)
 cis = empty_row_indexes(galaxy.transpose)
 galaxy.each_index {|y| galaxy[y].each_index {|x| gps << [x, y] if is_galaxy?(galaxy[y][x])}}
+s1 = Process.clock_gettime(Process::CLOCK_MONOTONIC, :nanosecond)
 p sum_dists(galaxy, gps, ris, cis, 2)
+e1 = Process.clock_gettime(Process::CLOCK_MONOTONIC, :nanosecond)
+s2 = Process.clock_gettime(Process::CLOCK_MONOTONIC, :nanosecond)
 p sum_dists(galaxy, gps, ris, cis, 1000000)
+e2 = Process.clock_gettime(Process::CLOCK_MONOTONIC, :nanosecond)
+e = Process.clock_gettime(Process::CLOCK_MONOTONIC, :nanosecond)
+puts "Time taken: part1: #{tfmt(e1-s1)}, part2: #{tfmt(e2-s2)}, total: #{tfmt(e-s)}"
